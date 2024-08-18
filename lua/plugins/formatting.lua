efbb33b352b1fd7626d56ef488651f174dd0f7f7
coffinggx <coffinggx@gmail.com>
2024-08-18 04:03:36 +0000

    Neovim setup

---
+++ lua/plugins/formatting.lua
@@ -0,0 +1,41 @@
+return {
+	"stevearc/conform.nvim",
+	lazy = true,
+	event = { "BufReadPre", "BufNewFile" }, -- to disable, comment this out
+	config = function()
+		local conform = require("conform")
+
+		conform.setup({
+			formatters_by_ft = {
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescriptreact = { "prettier" },
+				svelte = { "prettier" },
+				css = { "prettier" },
+				html = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+				graphql = { "prettier" },
+				lua = { "stylua" },
+				terraform = { "terraform_fmt" },
+				python = { "isort", "black" },
+				rust = { "rustfmt" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = true,
+				timeout_ms = 1000,
+			},
+		})
+
+		vim.keymap.set({ "n", "v" }, "<leader>ff", function()
+			conform.format({
+				lsp_fallback = true,
+				async = true,
+				timeout_ms = 1000,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
+	end,
+}
